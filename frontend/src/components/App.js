a44b2d46e01e0517fbce6968f9e6db432301f121
christoph lauper <christoph.lauper@gmail.com>
2018-09-05 10:07:46 +0200

    woke commit

---
+++ frontend/src/components/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Panel } from 'react-bootstrap/lib';
+import Style from '../util/Style.js';
+import Urls from '../util/Urls.js';
+import TopNavbar from './TopNavbar.js';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      windowWidth: window.innerWidth,
+      errors: [],
+    };
+  }
+
+  render() {
+    const { windowWidth, posts } = this.state;
+    let width;
+    if (windowWidth < Style.xsCutoff) {
+      width = '100%';
+    } else if (windowWidth < Style.smCutoff) {
+      width = '723px';
+    } else if (windowWidth < Style.mdCutoff) {
+      width = '933px';
+    } else {
+      width = '1127px';
+    }
+
+    const panelStyle = {
+      width,
+      margin: 'auto',
+      marginTop: '65px',
+    };
+
+    return (
+      <div>
+        <TopNavbar />
+        <Panel style={panelStyle} bsStyle="primary">
+          <h2>Welcome to Livy</h2>
+        </Panel>
+      </div>
+    );
+  }
+}
+
+export default App;
