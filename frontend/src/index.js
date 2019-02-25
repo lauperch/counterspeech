d6ca9911379f8ceaf4cfb4b4cecbe18fe5e7b99e
christoph lauper <christoph.lauper@gmail.com>
2019-02-25 23:26:29 +0100

    pls no node modules

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
-import App from './components/App';
+import {render} from 'react-dom';
+import './index.css';
+import App from './App';
 
-ReactDOM.render((
-  <Router history={browserHistory}>
-    <Route path="/" component={App} />
-  </Router>
-  ), document.getElementById('root'),
-);
+render(<App />, document.getElementById('root'));
