1890586b03ed06738cb74ea0775bb35879868718
christoph lauper <christoph.lauper@gmail.com>
2019-02-27 22:58:55 +0100

    added submit handler

--- api/api.go
+++ api/api.go
@@ -2,44 +2,58 @@ package main
 
 import (
 	"database"
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/julienschmidt/httprouter"
-	"gopkg.in/mgo.v2/bson"
+	"gopkg.in/mgo.v2"
 )
+
+var texts *mgo.Collection
 
 type Text struct {
 	Content string
 	URL     string
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func submit(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	setCors(w)
-	mySession := database.Session.Copy()
-	defer mySession.Close()
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		responseError(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
-	c := mySession.DB("test").C("myCollection")
+	text := &Text{}
+	err = json.Unmarshal(data, text)
+	if err != nil {
+		responseError(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
-	err := c.Insert(
-		&Text{"Lorem Ipsum", "http://a.com"},
-		&Text{"Dolor", "http://b.com"})
+	// Insert new post
+	if err := texts.Insert(text); err != nil {
+		responseError(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
+	responseJSON(w, text)
+
+}
+
+func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	setCors(w)
+	result := []Text{}
+	err := texts.Find(nil).All(&result)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
 	}
 
-	result := Text{}
-	err = c.Find(bson.M{"url": "http://b.com"}).One(&result)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-
-	fmt.Fprintf(w, result.Content)
+	responseJSON(w, result)
 }
 
 // used for COR preflight checks
@@ -63,19 +77,31 @@ func setCors(w http.ResponseWriter) {
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 }
 
+func responseJSON(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
+}
+
+func responseError(w http.ResponseWriter, message string, code int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(map[string]string{"error": message})
+}
+
 // Temporary Canary test to make sure Travis-CI is working
 func Canary(word string) string {
 	return word
 }
 
 func main() {
-	// add database
 	session, err := database.Init()
 	defer session.Close()
 
-	// add router and routes
+	texts = session.DB("app").C("texts")
+
 	router := httprouter.New()
-	router.GET("/", indexHandler)
+	router.GET("/", index)
+	router.POST("/submit", submit)
 
 	if err != nil {
 		log.Println("connection to mongodb failed, aborting...")
@@ -84,7 +110,6 @@ func main() {
 
 	log.Println("connected to mongodb")
 
-	// print env
 	env := os.Getenv("APP_ENV")
 	if env == "prod" {
 		log.Println("Running api server in prod mode")
