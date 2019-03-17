c5611444c29f7f22eff8ef24465bf324593d48b9
christoph lauper <christoph.lauper@gmail.com>
2019-03-17 07:21:46 +0100

    scrapeloop prod url

--- scripts/scrapeLoop.go
+++ scripts/scrapeLoop.go
@@ -7,13 +7,24 @@ import (
 
 func main() {
 	log.Println("Starting scrape loop...")
-	go run("https://www.20min.ch/schweiz/")
+	run("https://www.20min.ch/schweiz/")
+	run("https://www.20min.ch/wissen/")
+	run("https://www.20min.ch/digital/")
+	run("https://www.20min.ch/entertainment/")
+	run("https://www.20min.ch/people/")
 }
 
 func run(startUrl string) {
-	r, err := http.Get("https://ddd/run/www.20min.ch?startUrl=" + startUrl)
+	r, err := http.Get("http://35.198.123.101:3030/run/www.20min.ch?startUrl=" + startUrl)
 	if err != nil {
 		log.Println(err.Error())
 		return
 	}
+	log.Println(r)
+	r, err = http.Get("http://35.198.123.101:3030/stop/www.20min.ch")
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	log.Println(r)
 }
